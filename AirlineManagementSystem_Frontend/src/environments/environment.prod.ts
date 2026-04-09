47621264dabc1a0f0bd140cd713560ea49fed094
Aryan Narayan <aryanshrivastava788@gmail.com>
2026-04-09 11:35:01 +0530

    Airline Management System

---
+++ AirlineManagementSystem_Frontend/src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  apiBaseUrl: 'http://localhost:5000',
+  clerkPublishableKey: import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+};
---
+++ AirlineManagementSystem_Frontend/src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  apiBaseUrl: 'http://localhost:5000',
+  clerkPublishableKey: import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+};
